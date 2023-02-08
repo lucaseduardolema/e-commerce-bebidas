e52e6a216d3749115bcb1ef3d4e701d18a356be9
Lucas <lucaseduardolema@gmail.com>
2023-02-08 09:19:39 -0300

    chore: adiciona texto erro loginForm

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,16 +1,24 @@
 import { Formik } from 'formik';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, InputGroup } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import { loginFormSchema } from '../schemas/loginForm';
 import { postLogin } from '../services/auth.service';
 
 export default function LoginForm() {
+  const navigate = useNavigate()
   return (
     <>
       <Formik
         validationSchema={loginFormSchema}
         onSubmit={async (values, actions) => {
-          await postLogin(values)
-          actions.setSubmitting(false);
+          try {
+            await postLogin(values)
+            navigate('/customer/products')
+          } catch (error) {
+            console.error(error);
+          } finally {
+            actions.setSubmitting(false);
+          }
         }}
         initialValues={{
           email: '',
@@ -21,19 +29,24 @@ export default function LoginForm() {
           <Form noValidate onSubmit={handleSubmit}>
             <Form.Group className='mb-3' controlId='formEmail'>
               <Form.Label>Email</Form.Label>
-              <Form.Control
-                type='email'
-                placeholder='Insira seu email'
-                value={values.email}
-                onChange={handleChange}
-                name='email'
-                isValid={touched.email && !errors.email}
-                isInvalid={!!errors.email}
-              />
-              <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
-              <Form.Control.Feedback type="invalid" tooltip>
+              <InputGroup hasValidation>
+                <Form.Control
+                  required
+                  type='email'
+                  placeholder='Insira seu email'
+                  value={values.email}
+                  onChange={handleChange}
+                  name='email'
+                  isValid={touched.email && !errors.email}
+                  isInvalid={!!errors.email}
+                />
+                <Form.Control.Feedback tooltip>
+                  Looks good!
+                </Form.Control.Feedback>
+                <Form.Control.Feedback type='invalid' tooltip>
                   {errors.email}
                 </Form.Control.Feedback>
+              </InputGroup>
               <Form.Text className='text-muted'>
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -41,23 +54,27 @@ export default function LoginForm() {
 
             <Form.Group className='mb-3' controlId='formPassword'>
               <Form.Label>Password</Form.Label>
-              <Form.Control
-                type='password'
-                placeholder='Password'
-                value={values.password}
-                onChange={handleChange}
-                name='password'
-                isValid={touched.password && !errors.password}
-                isInvalid={!!errors.password}
-              />
-              <Form.Control.Feedback tooltip>Looks good!</Form.Control.Feedback>
-              <Form.Control.Feedback type="invalid" tooltip>
+              <InputGroup hasValidation>
+                <Form.Control
+                  required
+                  type='password'
+                  placeholder='Password'
+                  value={values.password}
+                  onChange={handleChange}
+                  name='password'
+                  isValid={touched.password && !errors.password}
+                  isInvalid={!!errors.password}
+                />
+                <Form.Control.Feedback tooltip>
+                  Looks good!
+                </Form.Control.Feedback>
+                <Form.Control.Feedback type='invalid' tooltip>
                   {errors.password}
                 </Form.Control.Feedback>
+              </InputGroup>
             </Form.Group>
 
             <Button type='submit'>Enviar</Button>
-
           </Form>
         )}
       </Formik>
